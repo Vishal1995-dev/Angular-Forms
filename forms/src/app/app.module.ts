1ccb99b1659c55e808a85a6c0ab24adda4d49d57
Vishal <bhandarivs19.comp@coep.ac.in>
2021-04-23 12:59:39 +0530

    Initial commit

---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoFormSkuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
