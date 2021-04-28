979a88f9bd7b1ffe234363412c1db0bd87a582f5
Vishal <bhandarivs19.comp@coep.ac.in>
2021-04-28 15:25:47 +0530

    Added custom validation and used ngModel

--- forms/src/app/form-with-builder/form-with-builder.component.ts
+++ forms/src/app/form-with-builder/form-with-builder.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+
+function nameValidator(control): { [s: string]: boolean } {
+	if (!control.value.match(/^123/)) {
+		return {invalidName: true};
+	}
+}
 
 @Component({
   selector: 'app-form-with-builder',
   templateUrl: './form-with-builder.component.html'
 })
+
 export class FormWithBuilderComponent implements OnInit {
   
   myForm:FormGroup;
+  name:AbstractControl;
+  Name:string;
   
   constructor(fb:FormBuilder) {
   	this.myForm=fb.group({
-  		'name':['Vishal']
+  		'name':['',Validators.compose([Validators.required,nameValidator])]
   	});	
+  	this.name=this.myForm.controls['name'];
+	
+	this.name.valueChanges.subscribe(
+	(value: string) => {
+		console.log('Name changed to:', value);
+	}
+	);
+	
+	this.myForm.valueChanges.subscribe(
+	(form: any) => {
+		console.log('Form changed to:', form);
+	}
+	);  
+  
   }
 
   ngOnInit(): void {
